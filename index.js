3c9f4747dc15402b82879f6ff27d4b2fe8ea6bbc
Unknown <alee12131415@gmail.com>
2019-01-03 00:09:37 -0800

    update babel + add gzip

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const compression = require('compression')
 const path = require('path')
 
 let app = express()
 
 app.set('port', app.get('env') === 'production' ? process.env.PORT ? process.env.PORT : 3000 : 8000)
+app.use(compression())
 
 app.use('/public', express.static('dist'))
 
--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'tictacturing',
+            title: 'React Express SPA Boilerplate',
             filename: './index.html',
             template: './src/index.html'
         })
